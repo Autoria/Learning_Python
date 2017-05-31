e8d3f10268f11cf0ebe9de3cb40c8f1ecf480aae
Autoria <578227262@qq.com>
2017-05-31 23:17:06 +0800

    orm

--- base_idea.py
+++ base_idea.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Liu_100'
 
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func
+    return wrapper
+
+@log
+def now():
+    print('2015-3-25')
+
+
+print(now.__name__)
+print(now().__name__)
