711790244c9c9fee5f9a5ee9afc0f1a3f73e937e
Autoria <578227262@qq.com>
2017-06-02 00:16:05 +0800

    orm

--- orm.py
+++ orm.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Liu_100'
 
-class User(Model):
-    id = IntegerField('id')
-    name = StringField('username')
-    email = StringField('email')
-    password = StringField('password')
 
 class Field():
 
@@ -30,16 +25,67 @@ class ModelMetaclass(type):
 
     def __new__(cls, name, bases, attrs):
         if name == 'Model':
-            return type.__new__(cls, name, bases, attrs)
+            return super().__new__(cls, name, bases, attrs)
+
         print('Found model: %s' % name)
         mappings = dict()
         for k, v in attrs.items():
             if isinstance(v, Field):
+                print('Found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
 
         for k in mappings.keys():
             attrs.pop(k)
         attrs['__mappings__'] = mappings
-        attrs['__table__] = name
+        attrs['__table__'] = name
 
-        return type.__new__(cls, name, bases, attrs)
+        return super().__new__(cls, name, bases, attrs)
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kwargs):
+        super(Model, self).__init__(**kwargs)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError('Model object has no attribute: %s' %key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def save(self):
+        fields = list()
+        params = list()
+        args = list()
+        for k, v in self.__mappings__.items():
+            fields.append(v.name)
+            params.append('?')
+            args.append(getattr(self, k, None))
+        sql = 'insert into %s (%s) values (%s)' % (self.__table__, ','.join(fields), ','.join(params))
+        print('SQL: %s' % sql)
+        print('ARGS: %s' % str(args))
+
+class User(Model):
+    id = IntegerField('id')
+    name = StringField('username')
+    email = StringField('email')
+    password = StringField('password')
+
+u = User(id=12345, name='Micheal', email='test@orm.org', password='my-pwd')
+u.save()
+
+
+
+# class Fuck(dict):
+#     a = 2222
+#     def __setattr__(self, key, value):
+#         self[key] = value
+#     def __getattr__(self, item):
+#         return self[item]
+#
+# fuck = Fuck()
+# fuck.a = 1
+# print(fuck.a)
+# print(fuck[''])
